37d789a2a66f1909d4bd82dd4319d378b1ef33d9
luislpzr <lpzr97@gmail.com>
2020-10-03 13:37:25 -0500

    First Exercise finished

---
+++ exeA.cpp
@@ -0,0 +1,17 @@
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+     int n;
+     cin >> n;
+     unordered_set<string> set;
+     for(int i=0; i < n; i++)
+     {
+          string temp;
+          cin >> temp;
+          set.insert(temp);
+     }
+     cout << set.size();
+}
