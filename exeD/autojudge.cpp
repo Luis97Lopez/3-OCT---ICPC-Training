3d423ce0db787c802afab09ed8e2aa27a49dcf05
MickeyMenor <miguel.menor09@gmail.com>
2020-10-16 18:12:15 -0500

    Programa D
    foo
    
    Signed-off-by: MickeyMenor <miguel.menor09@gmail.com>

--- exeD/autojudge.cpp
+++ exeD/autojudge.cpp
@@ -6,9 +6,27 @@ bool isFilenameValid(string filename, string submittedFile)
 {
     bool res = false;
     auto it = submittedFile.find_last_of('/');
-    string substring = submittedFile.substr(it, submittedFile.length() - 1);
+    
+    if (it != -1)
+        submittedFile = submittedFile.substr(it + 1, submittedFile.length());
+    
+    auto pos2 = submittedFile.find_last_of('.');
+    
+    if (pos2 == -1)
+        return false;
 
+    string extension = submittedFile.substr(pos2, submittedFile.length());
 
+    if (extension.compare(".c") == 0 || extension.compare(".cpp") == 0
+        || extension.compare(".java") == 0 || extension.compare(".py") == 0)
+    {
+        res = true;
+
+        if (filename.compare(submittedFile.substr(0, pos2 - 1)) == 0)
+            res = true;
+        else
+            res = false;
+    }
 
     return res;
 }
@@ -34,5 +52,6 @@ int main()
 
     for (int i = 0; i < t; i++)
         cin >> submittedLines[i];
-}
 
+    isFilenameValid(filename, submittedFile);
+}
