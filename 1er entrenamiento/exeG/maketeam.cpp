0a865f7c9d8cada14684e01c0cc27533cb741986
Luis97Lopez <lpzr97@gmail.com>
2020-10-31 11:30:59 -0600

    Cambios de luis

--- 1er entrenamiento/exeG/maketeam.cpp
+++ 1er entrenamiento/exeG/maketeam.cpp
@@ -11,7 +11,6 @@ bool search_answer(vector<int> camino, int costo){
      if(camino.size() == videos.size()){
           if(costo < res){
                res = costo;
-               
           }
           return true;
      }else{
@@ -23,6 +22,7 @@ bool search_answer(vector<int> camino, int costo){
                                    camino.push_back(i);
                                    bool temp = search_answer(camino, videos[i][j] + 1);
                                    camino.pop_back();
+
                                    if(!temp ){
                                         break;
                                    }
@@ -33,6 +33,15 @@ bool search_answer(vector<int> camino, int costo){
           }
      }
      return false;
+}
+
+void search_answer2(){
+     vector<int> camino = {};
+     int costo = 0;
+
+     while (true){
+          
+     }
 }
 
 
@@ -53,7 +62,7 @@ int main(){
      }
 
      search_answer({}, 0);
-
+     //search_answer2();
      cout << res << endl;
      return 0;
 }
